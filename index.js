32b3dc0be45fb6221a84d8bf603b485e9da497aa
axionos <axionoso@gmail.com>
2019-11-07 11:22:19 -0500

    regex expresson

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function validAnagram(str1, str2){
 
     const lookup = {}
 
-
     for(let char of str1){
         !!lookup[char] ? lookup[char]++ : lookup[char] = 1
     }
@@ -21,13 +20,27 @@ function validAnagram(str1, str2){
     }
 
     return true
-
 }
+
+function validAnagram2(stringA, stringB) {
+  return clean(stringA) === clean(stringB)
+}
+
+clean = str => {
+  return str.replace(/[^\w]/g, '').split('').sort().join('')
+}
+
+// validAnagram2('hello', 'you') // false
+// validAnagram2('rat', 'car') // false
+// validAnagram2('awesome', 'awesom') // false
+// validAnagram2('qwerty', 'qeywrt') // true
+validAnagram2('texttwisttime', 'timetwisttext') // true
+
 
 // validAnagram('', '') // true
 // validAnagram('aaz', 'zza') // false
 // validAnagram('anagram', 'nagaram') // true
-validAnagram('rat', 'car') // false
+// validAnagram('rat', 'car') // false
 // validAnagram('awesome', 'awesom') // false
 // validAnagram('qwerty', 'qeywrt') // true
-// validAnagram('texttwisttime', 'timetwisttext') // true
+validAnagram('texttwisttime', 'timetwisttext') // true
